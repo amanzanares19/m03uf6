5a9ceb066a6c04fd277aa1bba29bc2326dd4a316
amanzanares19 <amanzanares19@ilg.cat>
2023-05-19 18:49:12 +0200

    Updated bbdd

--- jdbc-demo-skeleton/src/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/daos/impl/jdbc/EmployeeDAOJDBCImpl.java
+++ jdbc-demo-skeleton/src/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/daos/impl/jdbc/EmployeeDAOJDBCImpl.java
@@ -11,7 +11,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
